fbea9b8070002c0cc7a8fb7d4e270977e751da69
charaekeow <charaekeow11@gmail.com>
2023-11-25 17:35:58 +0800

    FIX(database): Update language code to use ISO 639-1 code

--- database/seeds/seedData.ts
+++ database/seeds/seedData.ts
@@ -5,9 +5,10 @@ export async function seed(knex: Knex): Promise<void> {
 	await knex('language').del();
 
 	// Inserts seed entries
+	// Reference: https://en.wikipedia.org/wiki/List_of_ISO_639-1_codes
 	await knex('language').insert([
-		{ id: 1, language_code: 'BM', language_name: 'Bahasa Malaysia' },
-		{ id: 2, language_code: 'EN', language_name: 'English' },
+		{ id: 1, language_code: 'ms', language_name: 'Bahasa Malaysia' },
+		{ id: 2, language_code: 'en', language_name: 'English' },
 	]);
 
 	await knex('publisher').del();
